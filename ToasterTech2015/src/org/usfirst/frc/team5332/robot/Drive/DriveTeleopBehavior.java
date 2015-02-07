716e3f94129a9da5a9df0bfc9b94fe6d37c6ea6a
Josh Dierberger <quantum.di@bellsouth.net>
2015-02-07 13:47:33 -0500

    Added tank drive, scaling variables, and a separate drive mode for slow driving speeds

--- ToasterTech2015/src/org/usfirst/frc/team5332/robot/Drive/DriveTeleopBehavior.java
+++ ToasterTech2015/src/org/usfirst/frc/team5332/robot/Drive/DriveTeleopBehavior.java
@@ -7,13 +7,34 @@ import org.usfirst.frc.team5332.robot.SystemBehavior;
 import edu.wpi.first.wpilibj.Joystick;
 
 public class DriveTeleopBehavior extends SystemBehavior<DriveSoftware>{
-	private Joystick joystick;
+	private Joystick joystick1;
+	private Joystick joystick2;
+	//Return values
+	private Double JSLeftReturn; //Left Joystick's returned value
+	private Double JSRightReturn; //Right Joystick's returned value
+	private Double JSLeftScaling; //Scaling processing variable
+	private Double JSRightScaling; //Scaling processing variable
+	private Double n; //Scaling number
 	public DriveTeleopBehavior(DriveSoftware sw) {
 		super(sw);
-		joystick=IO.joystick;
+		joystick1=IO.joystick1;
+		joystick2=IO.joystick2;
+		JSLeftReturn=joystick1.getY(); //Speed scaling can be done here
+		JSRightReturn=joystick2.getY(); //Speed scaling can be done here
+		n = 2.0; //Scaling number (JSReturnedValue / n)
 	}
 	@Override
 	public void run() {
-		this.software.driveMotors(joystick.getX(), joystick.getY());
+		this.software.driveMotors(joystick1.getX(), joystick1.getY()); //Speed controlled by different axis(es)
+	}
+	
+	public void tankDrive(){
+		this.software.driveMotors(JSLeftReturn, JSRightReturn); //Speed controlled by Y of two joysticks
+	}
+	
+	public void lowSpeedDrive(){
+		JSLeftScaling = JSLeftReturn / n; //Get scaled value
+		JSRightScaling = JSRightReturn / n; //Get scaled value
+		this.software.driveMotors(JSLeftScaling, JSRightScaling); //Tank drive with new speed values
 	}
 }
--- ToasterTech2015/src/org/usfirst/frc/team5332/robot/IO.java
+++ ToasterTech2015/src/org/usfirst/frc/team5332/robot/IO.java
@@ -6,5 +6,6 @@ import edu.wpi.first.wpilibj.TalonSRX;
 public class IO {
 	public final static TalonSRX driveLeft1=new TalonSRX(1);
 	public final static TalonSRX driveLeft2=new TalonSRX(2);
-	public final static Joystick joystick=new Joystick(1);
+	public final static Joystick joystick1=new Joystick(1);
+	public final static Joystick joystick2=new Joystick(2);
 }
